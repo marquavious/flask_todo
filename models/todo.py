5831e6bd177b8db3f477f2b912af9dd90f7453e2
Marquavious Draggon <marq.draggon@gmail.com>
2017-07-06 15:45:33 -0400

    added Resources

--- models/todo.py
+++ models/todo.py
@@ -7,10 +7,13 @@ class TodoModel(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    category_id = db.Column(db.Integer, db.ForeignKey.('category.id'))
+    category = db.relationship('CategoryModel')
 
-    def __init__(self,title,description):
+    def __init__(self,title,description,category_id):
         self.title = title
         self.description = description
+        self.category_id = category_id
 
     def json(self):
         return {'name':self.title, 'description':self.description}
--- resources/category.py
+++ resources/category.py
@@ -5,7 +5,7 @@ class Category(Resource):
 
     parser = reqparse.RequestParser()
 
-    parser.add_argument('title',
+    parser.add_argument('name',
         type = str,
         required = True,
         help="Every Category must have a name!"
@@ -14,14 +14,34 @@ class Category(Resource):
     def __init__(self,name):
         self.name = name
 
-    def get():
-        pass
+    def get(self,name):
+        category = CategoryModel.find_by_name(name)
 
-    def post():
-        pass
+        if category:
+            return category.json()
+        return {'message':"No category with the name of'{}' exists".format(name)}
 
-    def put():
-        pass
+    def post(self,name):
+        if CategoryModel.find_by_name(name):
+            return {'message':"A category with the name of '{}' already exists".format(name)}
 
-    def delete():
-        pass
+        data = parser.parse_args()
+
+        category = CategoryModel(data["name"])
+
+        try:
+            category.save_to_db()
+        except:
+            return {'message':"A error occured wil creating the category."}, 500
+
+        return {'message': "Category '{}' was created".format(name)}, 201
+
+    def delete(self,name):
+        category = Category.find_by_name(name)
+        if category:
+            category.delete_from_db()
+        return {'message':'Deleted the category'}
+
+class CategoryList(Resource):
+    def get(self):
+        return{'Categories':list(map(lambda x: x.json(), in CategoryModel.query.all()))}
